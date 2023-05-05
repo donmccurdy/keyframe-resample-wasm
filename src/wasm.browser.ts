9ed82ea62c8272cd5437405b9938a0cec4a6a548
Don McCurdy <dm@donmccurdy.com>
2023-05-05 14:44:19 -0400

    trying again

--- src/wasm.browser.ts
+++ src/wasm.browser.ts
@@ -1,3 +1,3 @@
 /** WASM loader for Web environments. */
-const wasm = /* #__PURE__ */ fetch(new URL('./release.wasm', import.meta.url));
+const wasm = /* #__PURE__ */ fetch(/* #__PURE__ */ new URL('./release.wasm', import.meta.url));
 export default wasm;
