7dd6bca2ff94268cf3f96c087cdcb6c9edba4cdf
Don McCurdy <dm@donmccurdy.com>
2024-01-05 11:46:21 -0600

    Fix errors in wasm providers

--- src/wasm.browser.ts
+++ src/wasm.browser.ts
@@ -1,3 +1,7 @@
 /** WASM loader for Web environments. */
-const wasm = /* #__PURE__ */ fetch(/* #__PURE__ */ new URL('./release.wasm', import.meta.url));
+const wasm: Promise<Uint8Array> = /* #__PURE__ */ fetch(
+	/* #__PURE__ */ new URL('./release.wasm', import.meta.url)
+)
+	.then((response) => response.arrayBuffer())
+	.then((buffer) => new Uint8Array(buffer));
 export default wasm;
