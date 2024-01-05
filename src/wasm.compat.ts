9fcf2f767376003cd8bbc6746a207f06d06ed929
Don McCurdy <dm@donmccurdy.com>
2024-01-05 11:46:21 -0600

    Fix errors in wasm providers

--- src/wasm.compat.ts
+++ src/wasm.compat.ts
@@ -1,4 +1,8 @@
 /** WASM loader using an inline Data URI. */
 const WASM_BASE64 = '';
-const wasm = /* #__PURE__ */ fetch('data:application/wasm;base64' + WASM_BASE64);
+const wasm: Promise<Uint8Array> = /* #__PURE__ */ fetch(
+	'data:application/wasm;base64' + WASM_BASE64
+)
+	.then((response) => response.arrayBuffer())
+	.then((buffer) => new Uint8Array(buffer));
 export default wasm;
--- src/wasm.node.ts
+++ src/wasm.node.ts
@@ -3,5 +3,5 @@ import { readFile } from 'node:fs/promises';
 /** WASM loader for Node.js environments. */
 
 const wasmURL = /* #__PURE__ */ new URL('./release.wasm', import.meta.url);
-const wasm = /* #__PURE__ */ readFile(wasmURL);
+const wasm: Promise<Uint8Array> = /* #__PURE__ */ readFile(wasmURL);
 export default wasm;
